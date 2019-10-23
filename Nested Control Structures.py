030bed0d92758d38e2cc0e094acffd5c21be0a2e
Wyatt McComb <02wyatt.mccomb@gulllakecs.org>
2019-10-23 10:56:58 -0700

    Initial Commit - For Loop + While Loop

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -10,3 +10,15 @@ for i in range(3):
     print('Outer For Loop: ' + str(i))
     for l in range(2):
         print("     Inner For Loop: " + str(l))
+
+
+print(\n***************\n)
+
+'''
+Programmer: Wyatt McCXomb
+Date 10/22/19
+Program: For Loop + While Loop
+
+This program will ask users of an internet to them then ask for two items related to that interest
+'''
+
